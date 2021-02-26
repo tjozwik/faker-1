8de4ff7d625b3049c9321c45c693e57ee3293477
Tomasz Jóźwik <tjozwik@protonmail.com>
2021-02-26 13:51:33 +0100

    Fix wrong condition for generating usernames

--- src/faker/internet.cr
+++ src/faker/internet.cr
@@ -46,7 +46,7 @@ module Faker
         return result[0...specifier.max]
       end
 
-      return name.scan(/\w+/).shuffle(Faker.rng).map(&.[0]).join(separators.sample(Faker.rng)).downcase if name
+      return name.scan(/\w+/).shuffle(Faker.rng).map(&.[0]).join(separators.sample(Faker.rng)).downcase if specifier.responds_to?(:scan)
       [
         ->{ Name.first_name.gsub(/\W/, "").downcase },
         ->{
